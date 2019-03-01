ece527b7c1feddbeeeba8ab87715aa7b64acea61
NikulinIlya <ilia.nikulin@gmail.com>
2019-03-01 18:43:11 +0300

    build: install material-design-icons-iconfont

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,11 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
