80abed088c81f0a8f303df8dbb6220e219a87038
NikulinIlya <ilia.nikulin@gmail.com>
2019-03-01 20:18:14 +0300

    feat(components): make Redirect.vue

---
+++ frontend/src/components/buttons/Redirect.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn type="button" @click="goTo">Rock Stars</v-btn>
+</template>
+
+<script>
+
+export default {
+  props:{
+    pathname: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    goTo() {
+      this.$router.push({name: this.pathname})
+    }
+  }
+}
+</script>
